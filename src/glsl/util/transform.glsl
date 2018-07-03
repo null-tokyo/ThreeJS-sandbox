e12a8b691987fe0f9fa6d54ad40b0bf20694048c
null <nuuullnull@gmail.com>
2018-07-03 20:57:10 +0900

    scaleの修正

--- src/glsl/util/transform.glsl
+++ src/glsl/util/transform.glsl
@@ -3,7 +3,7 @@ vec2 rotate2d(vec2 st, float _angle){
                 sin(_angle),cos(_angle));
 }
 
-vec2 scale2d(vec2 st, float _scale){
-    return st * return mat2(_scale.x,0.0,
+vec2 scale2d(vec2 st, vec2 _scale){
+    return st * mat2(_scale.x,0.0,
                             0.0,_scale.y);
 }
